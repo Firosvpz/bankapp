443e472adc2fd4e0bf43664101aa9f5f124280dd
Firosvpz <vpzfiroz@gmail.com>
2022-12-08 11:13:33 +0530

    login created

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  aim='Your banking partner'
+  acnt="enter your acc number"
+  
+  acno=''
+  pswd=''
+
+  userDetails:any={
+    1000:{acno:1000,username:'amal',password:123,balance:100000},
+    1001:{acno:1001,username:'ami',password:123,balance:200000},
+    1002:{acno:1002,username:'joyal',password:123,balance:300000},
+    1003:{acno:1003,username:'amaldas',password:123,balance:500000},
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    var acno=this.acno
+    var pswd=this.pswd
+    let userDetails=this.userDetails
+    if(acno in userDetails){
+      if(pswd==userDetails[acno]['password']){
+          alert('login success')
+
+      }
+      else{
+        alert('incorrect password')
+      }
+    }
+    else{
+      alert('user not exist or incorrect password')
+    }
+
+    alert("clicked")
+  }
+ acnoChange(event:any){
+    this.acno=event.target.value
+    console.log(this.acno);
+    
+    
+ }
+ changePswd(event:any){
+   this.pswd=event.target.value
+   console.log(this.pswd);
+   
+ }
+}
